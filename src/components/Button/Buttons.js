c8ebe5c1889b45177699bd83ef7bc570ba065731
temurbek0940 <temurbekanvarov0956@gmail.com>
2023-02-16 14:53:00 +0500

    react file

---
+++ src/components/Button/Buttons.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Buttons({title}, {hendClick} ) {
+    function btn() {}
+    return <button className="btn"></button>;
+    {
+        title;
+    }
+    {
+        hendClick;
+    }
+}
+
+export default Buttons;
